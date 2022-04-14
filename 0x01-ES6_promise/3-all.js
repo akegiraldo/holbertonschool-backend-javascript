f96915f19f6c804bcb8c335ae07075b024f94254
akegiraldo <ake.giraldo@gmail.com>
2022-04-13 23:51:01 -0500

    [task 3] - Fix try catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,11 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      if (values[0].status === 'fulfilled' && values[1].status === 'fulfilled') {
-        console.log(values[0].body, values[1].firstName, values[1].lastName);
-        return;
-      }
-      throw Error();
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
