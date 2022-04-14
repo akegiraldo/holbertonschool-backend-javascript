71b76647de2870204218ed57dbf2a7ad8b57422a
akegiraldo <ake.giraldo@gmail.com>
2022-04-13 22:25:44 -0500

    [task 2] - Fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,8 @@
-/* eslint-disable func-names */
-/* eslint-disable prefer-arrow-callback */
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(function () {
-      return { status: 200 };
-    })
-    .catch(function () {
-      return Error();
-    })
-    .finally(function () {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => {
       console.log('Got a response from the API');
     });
 }
