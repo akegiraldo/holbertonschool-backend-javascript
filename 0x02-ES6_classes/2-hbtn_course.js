4c39d83740479c2f656bfec7c95fcd3fb1852d77
akegiraldo <ake.giraldo@gmail.com>
2022-04-14 22:06:43 -0500

    [task 2] - Fix getters and setters names

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,49 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an array');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  set _name(name) {
+  set name(name) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
     }
     this._name = name;
   }
 
-  get _name() {
+  get name() {
     return this._name;
   }
 
-  set _length(length) {
+  set length(length) {
     if (typeof length !== 'number') {
       throw TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  get _length() {
+  get length() {
     return this._length;
   }
 
-  set _students(students) {
+  set students(students) {
     if (typeof students !== 'object') {
       throw TypeError('Students must be an array');
     }
     this._students = students;
   }
 
-  get _students() {
+  get students() {
     return this._students;
   }
 }
